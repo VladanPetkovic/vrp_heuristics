601ec698aafb60f11e6828c36f4783df36436e4c
petkovic <vladan.petkovic27@gmail.com>
2025-03-22 16:11:53 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    auto language = "C++";
+    std::cout << "Hello and welcome to " << language << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
